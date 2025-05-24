44156627da1c0121dc0274ebe5ca982e0c42a554
silvakeci <>
2025-05-24 17:14:28 +0200

    changes

--- myportfolio/src/components/heroSection/HeroPic.jsx
+++ myportfolio/src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/sil.png"
+        src="../../images/sil.png"
         alt="Silva Keci"
         className="max-h-[450px] w-auto"
       />
